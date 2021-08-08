d484612684f5262f4401784f0c68ae114897e107
Рамазан Айсаев <aisaev-68@yandex.ru>
2021-08-08 20:03:20 +0300

    исправление 4 задачи

--- Module14/04_reverse_num/main.py
+++ Module14/04_reverse_num/main.py
@@ -10,7 +10,7 @@ def revers_numb(n_var):
 
 
 def f_fraction(a):
-    return int(round(a - math.floor(a), 2) * 100)
+    return int(round(a - math.floor(a), 15) * 1e15)
 
 
 def whole_part(b):
@@ -20,8 +20,8 @@ def whole_part(b):
 first_minus = sec_minus = ''
 n = float(input('Введите первое число: '))
 k = float(input('Введите второе число: '))
-n = round(n, 2)
-k = round(k, 2)
+n = round(n, 15)
+k = round(k, 15)
 if n < 0:
     n = - n
     first_minus = '-'
@@ -30,6 +30,7 @@ elif k < 0:
     sec_minus = '-'
 first_numb = first_minus + revers_numb(whole_part(n)) + '.' + revers_numb(f_fraction(n))
 sec_numb = sec_minus + revers_numb(whole_part(k)) + '.' + revers_numb(f_fraction(k))
-print('\nПервое число наоборот: ', first_numb)
-print('Второе число наоборот: ', sec_numb)
+print('\nПервое число наоборот: ', round(float(first_numb), 2))
+print('Второе число наоборот: ', round(float(sec_numb), 2))
 print('Сумма:', round(float(first_numb) + float(sec_numb), 2))
+
