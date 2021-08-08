30102ab8f6c5e1d11afa53aca5868da7d272c736
Рамазан Айсаев <aisaev-68@yandex.ru>
2021-08-08 21:10:04 +0300

    задачи 15 модуля

--- Module15/03_cells/main.py
+++ Module15/03_cells/main.py
@@ -1,1 +1,11 @@
-# TODO здесь писать код
+n_cells = int(input('Кол-во клеток: '))
+bad_cells = []
+for i in range(n_cells):
+    print('Эффективность', i + 1, 'клетки: ', end='')
+    efficiency = int(input())
+    if i >= efficiency:
+        bad_cells.append(efficiency)
+
+print('Неподходящие значения: ', end='')
+for j in bad_cells:
+    print(j, end=' ')
