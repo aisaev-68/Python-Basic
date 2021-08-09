236f44874e49cc2253043c76b2e9edae95b2d8e8
Рамазан Айсаев <aisaev-68@yandex.ru>
2021-08-09 22:41:39 +0300

    исправление задач 1, 5, 7 и 8 15 модуля

--- Module15/01_list_gen/main.py
+++ Module15/01_list_gen/main.py
@@ -2,7 +2,7 @@ def no_even(n):
     no_even_list = []
     for i in range(1, n + 1, 2):
         no_even_list.append(i)
-
+    return no_even_list
 
 numb = int(input('Введите целое число: '))
 if numb >= 1:
