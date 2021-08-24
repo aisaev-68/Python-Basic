d0b3f34d2fcb8d9106ea25bcd400382b35a234d6
Рамазан Айсаев <aisaev-68@yandex.ru>
2021-08-24 20:31:52 +0300

    исправление 6 и 7 задачи 19 модуля

--- Module19/06_synonym_dict/main.py
+++ Module19/06_synonym_dict/main.py
@@ -2,13 +2,13 @@ def input_synonym(numb):
     count = 0
     print('Введите слова через дефис.')
     while count < numb:
-        txt_syn = input(f'{count + 1} пара: ').lower()
-        txt_syn = txt_syn.split(' - ')
-        if txt_syn[0].title() in dict_syn.keys():
-            print(f'Слово {txt_syn[0].title()} содержится в словаре.\n')
+        txt_syn = input(f'{count + 1} пара: ').title()
+        word, syn = txt_syn.split(' - ')
+        if word in dict_syn.keys():
+            dict_syn[word].append(syn)
         else:
-            dict_syn[txt_syn[0].title()] = txt_syn[1].title()
-            count += 1
+            dict_syn[word] = [syn]
+        count += 1
     print(dict_syn)
 
 
@@ -20,7 +20,7 @@ def search_syn():
             if word == key:
                 logic = True
                 word = value
-            elif word == value:
+            elif word in value:
                 logic = True
                 word = key
         if logic:
--- Module19/07_pizza/main.py
+++ Module19/07_pizza/main.py
@@ -4,21 +4,17 @@ def input_order(numb):
     while count < numb:
         txt_order = input(f'{count + 1} заказ: ').title()
         if f_check_sym(txt_order):
-            txt_order = txt_order.split()
-            if not dict_order.get(txt_order[0]):
-                dict_order[txt_order[0]] = txt_order[0]
-                dict_order[txt_order[0]] = [{txt_order[1]: int(txt_order[2])}]
+            surname, name_pizza, number = txt_order.split()
+            if not dict_order.get(surname):
+                dict_order[surname] = surname
+                dict_order[surname] = {name_pizza: int(number)}
             else:
-                for key, value in dict_order.items():
-                    if key == txt_order[0]:
-                        for i_dict in value:
-                            if i_dict.get(txt_order[1]):
-                                summ = i_dict[txt_order[1]] + int(txt_order[2])
-                                i_dict.update({txt_order[1]: summ})
-                                break
-                            else:
-                                value.append({txt_order[1]: int(txt_order[2])})
-                                break
+                if dict_order[surname].get(name_pizza):
+                    summ = dict_order[surname][name_pizza] + int(number)
+                    dict_order[surname][name_pizza] = int(summ)
+                else:
+                    dict_order[surname][name_pizza] = name_pizza
+                    dict_order[surname][name_pizza] = int(number)
         else:
             print('\nВ строке заказа должно быть два пробела\n')
             count -= 1
@@ -38,11 +34,11 @@ def f_check_sym(txt_str):
 
 def print_order():
     print()
-    for key, value in dict_order.items():
+    for key, value in sorted(dict_order.items()):
         print(f'{key}:')
-        for i_item in value:
-            for key1, value1 in i_item.items():
-                print(f'  {key1}: {value1}')
+        result = sorted(value, key=lambda x: x[0])
+        for key1 in sorted(result):
+            print(f'  {key1}: {value[key1]}')
 
 
 dict_order = dict()
