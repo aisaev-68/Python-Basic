211220fa7bee21441a458065bf6d61e554d6e392
Рамазан Айсаев <aisaev-68@yandex.ru>
2021-09-04 22:10:48 +0300

    1 задача 22 модуля

--- Module22/01_nums_sum_2/main.py
+++ Module22/01_nums_sum_2/main.py
@@ -1,13 +1,23 @@
+def print_contents(file):
+    with open(file, 'r', encoding='utf-8') as d_file:
+        print('\nСодержимое файла', file)
+        for i_elem in d_file.readlines():
+            print(i_elem, end='')
+        print()
+
+
 summ = 0
-with open('answer.txt', 'r+', encoding='utf-8') as answ_file:
+name_file = input('Введите имя выходного файла: ')
+with open(name_file + '.txt', 'w', encoding='utf-8') as answ_file:
     with open('number.txt', 'r', encoding='utf-8') as n_file:
-        print('Содержимое файла numbers.txt')
-        for i_numb in n_file:
-            print(f'{i_numb}', end='')
-            if ' '.join(i_numb.split()).isdigit():
-                summ += int(' '.join(i_numb.split()))
+        for i_numb in n_file.readlines():
+            if i_numb.endswith('\n'):
+                i_numb = i_numb[:len(i_numb)]
+            for dig in i_numb.split():
+                if dig.isdigit():
+                    summ += int(dig)
+                else:
+                    print(f'{dig} не является числом.')
         answ_file.write(str(summ))
-    answ_file.seek(0)
-    print('\n\nСодержимое файла answer.txt')
-    for i_answ in answ_file:
-        print(f'{i_answ}\n')
+print_contents(n_file.name)
+print_contents(answ_file.name)
