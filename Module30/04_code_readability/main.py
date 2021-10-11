e7c84789f9777dbfd2a7525798b8c7b864363685
Рамазан Айсаев <aisaev-68@yandex.ru>
2021-10-11 20:26:58 +0300

    30 задание

--- Module30/04_code_readability/main.py
+++ Module30/04_code_readability/main.py
@@ -1,15 +1,13 @@
 from typing import List
 
+
 if __name__ == '__main__':
     print('Необходимо вывести на экран список простых чисел от 0 до 1000. \nРеализуйте это двумя способами: ')
     print('1. «однострочным» кодом, без объявления дополнительных функций;')
-    print('1 способ: ', [x for x in range(1000)])
-    print('2 способ: ', list(range(1000)))
+    print('1 способ: ', list(map(int, filter(lambda x: all(map(lambda y: x % y != 0, range(2, x))), range(2, 1001)))))
+    print('2 способ: ', [i for i in range(2, 1001) if 0 not in [i % j for j in range(2, i)]])
 
-    print('1. обычным, «своим» кодом, который покажется вам наиболее красивым')
-    numbers: List[int] = []
-    for i in range(1000):
-        numbers.append(i)
-    print('3 способ: ', numbers)
+    print('2. обычным, «своим» кодом, который покажется вам наиболее красивым')
+    print([i for i in range(2, 1001) if 0 not in [i % j for j in range(2, i)]])
 
-    print('Мне удобен 1 способ')
+    print('Мне удобен 2 способ')
