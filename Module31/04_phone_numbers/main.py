93b340f9428e762798d87833626e8d0f888de8f3
Рамазан Айсаев <aisaev-68@yandex.ru>
2021-10-16 11:28:34 +0300

    31 задание

--- Module31/04_phone_numbers/main.py
+++ Module31/04_phone_numbers/main.py
@@ -3,5 +3,5 @@ import re
 if __name__ == '__main__':
     print('Результат:')
     for ind, item in enumerate(['9999999999', '999999-999', '99999x9999']):
-        print(f'{ind + 1} номер: все в порядке' if re.match(r'\d{10}',
+        print(f'{ind + 1} номер: все в порядке' if re.match(r'\b[8-9]{1}[0-9]{9}\b',
                                                             item) is not None else f'{ind + 1} номер: не подходит')
--- Module31/05_pin_code/main.py
+++ Module31/05_pin_code/main.py
@@ -1,7 +1,10 @@
 import itertools
 
 if __name__ == '__main__':
-    all_code = itertools.permutations([0, 1, 2, 3, 4, 5, 6, 7, 8, 9], r=4)
+    i = 0
+    all_code = itertools.product([0, 1, 2, 3, 4, 5, 6, 7, 8, 9], repeat=4)
     print('Все возможные коды:')
     for i_code in all_code:
+        i += 1
         print(i_code)
+    print(i)
